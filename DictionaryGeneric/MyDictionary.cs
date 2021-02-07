861584ff0982e2f7f11cc850848a096f722b49ac
Berke Karaman <berkekaraman99@hotmail.com>
2021-02-07 14:16:41 +0300

    Proje dosyası ekle.

---
+++ DictionaryGeneric/MyDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryGeneric
+{
+    class MyDictionary<K,V>
+    {
+        K[] Key;
+        V[] Value;
+        
+        public MyDictionary()
+        {
+            Key = new K[0];
+            Value = new V[0];
+        }
+
+        public void Add(K item1, V item2)
+        {
+            K[] tempKey = Key;
+            V[] tempValue = Value;
+            Key = new K[Key.Length + 1];
+            Value = new V[Value.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                Key[i] = tempKey[i];
+                Value[i] = tempValue[i];
+            }
+            Key[Key.Length - 1] = item1;
+            Value[Value.Length - 1] = item2;
+        }
+
+        public int Count
+        {
+            get { return Key.Length; }
+        }
+
+        public void List()
+        {
+            for (int i = 0; i < Key.Length; i++)
+            {
+                Console.WriteLine(Key[i] + "  " + Value[i]);
+            }
+        }
+    }
+}
---
+++ DictionaryGeneric/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DictionaryGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> City = new MyDictionary<int, string>();
+
+            City.Add(34, "İstanbul");
+            City.Add(35, "İzmir");
+            City.Add(1, "Adana");
+            City.Add(24, "Erzincan");
+            City.Add(6, "Ankara");
+
+            City.List();
+        }
+    }
+}
